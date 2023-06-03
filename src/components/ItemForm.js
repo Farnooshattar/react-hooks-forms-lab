2081b6a2d0bf5a24b738358ef9cc61378ebea1b8
Farnoosh Tavakkoli <attarfarnoosh@gmail.com>
2023-06-03 01:37:19 -0400

    updates a shopping list by controlled states

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,23 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
-  return (
-    <form className="NewItem">
-      <label>
-        Name:
-        <input type="text" name="name" />
-      </label>
+function ItemForm({ onItemFormSubmit }) {
+  const [itemName, setItemName] = useState("");
+  const [itemCategory, setItemCategory] = useState("Produce");
 
+  function handleNameChange(event) {
+    setItemName(event.target.value);
+  }
+
+  function handleCategoryChange(event) {
+    setItemCategory(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const newItem = {
+      id: uuid(),
+      name: itemName,
+      category: itemCategory,
+    };
+
+    onItemFormSubmit(newItem);
+
+    setItemName("");
+    setItemCategory("Produce");
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Item Name:
+        <input
+          type="text"
+          value={itemName}
+          onChange={handleNameChange}
+          placeholder="Add item..."
+        />
+      </label>
       <label>
         Category:
-        <select name="category">
+        <select value={itemCategory} onChange={handleCategoryChange}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
         </select>
       </label>
-
       <button type="submit">Add to List</button>
     </form>
   );
